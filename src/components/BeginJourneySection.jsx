c986eebc0e6af6be817e65f461743ced7dcd12d4
Siarhei <s.balabanowicz@gmail.com>
2026-04-28 22:13:53 +0100

    fix: restore GSAP animation and ensure safe client-side execution

--- src/components/BeginJourneySection.jsx
+++ src/components/BeginJourneySection.jsx
@@ -6,20 +6,20 @@ import curvedBackground from '../assets/wave_background.png';
 import logoAffini from '../assets/logo.png';
 
 function BeginYourFamilyJourney() {
-    const imageRef = useRef<HTMLImageElement | null>(null);
+    const imageRef = useRef(null);
 
-useEffect(() => {
-    if (typeof window === "undefined") return;
-    if (!imageRef.current) return;
+    useEffect(() => {
+        if (typeof window === "undefined") return;
+        if (!imageRef.current) return;
 
-    gsap.to(imageRef.current, {
-    y: 20,
-    duration: 3,
-    repeat: -1,
-    yoyo: true,
-    ease: "sine.inOut",
-    });
-}, []);
+        gsap.to(imageRef.current, {
+            y: 20,
+            duration: 3,
+            repeat: -1,
+            yoyo: true,
+            ease: "sine.inOut",
+        });
+    }, []);
     return (
         <section className="relative flex justify-center items-center bg-cover bg-top w-full min-h-[676px] overflow-visible"
             style={{ backgroundImage: `url(${curvedBackground})` }}
