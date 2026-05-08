2a98b493a4ca0529cbdd55c8ea273b182aacc818
Siarhei <s.balabanowicz@gmail.com>
2026-05-08 16:17:38 +0100

    refactor(testimonials-section): use design tokens and improve accessibility

--- src/components/TestimonialsSection.jsx
+++ src/components/TestimonialsSection.jsx
@@ -116,13 +116,13 @@ function TestimonialsSection() {
         className="
           pointer-events-none absolute z-0 opacity-80
           w-[500px] h-[395px]
-          left-[-95px] top-[500px] rotate-[50deg]
+          left-[-95px] top-[280px] rotate-[50deg]
           md:w-[395px]
           md:left-[-36px] md:top-[406px] md:rotate-[10deg]
           lg:w-[450px]
           lg:left-[-38px] lg:top-[80px] lg:rotate-[45deg]
           xl:w-[495px] xl:h-[770px]
-          xl:left-[-43px] xl:top-[54px] xl:rotate-[10deg]
+          xl:left-[-43px] xl:top-[180px] xl:rotate-[10deg]
         "
       />
 
