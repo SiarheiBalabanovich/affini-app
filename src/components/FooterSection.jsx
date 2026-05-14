081cba109664a835d529f6723f6bbfbe30e7b5c2
Siarhei <s.balabanowicz@gmail.com>
2026-05-14 15:04:05 +0100

    changed gap logo and menu in the footer

--- src/components/FooterSection.jsx
+++ src/components/FooterSection.jsx
@@ -15,7 +15,7 @@ function FooterSection() {
   return (
     <footer className="bg-primary text-whiteCustom">
       <div className="mx-auto max-w-[1280px] px-4 py-10 md:px-10 lg:px-16">
-        <div className="flex flex-col gap-10 lg:flex-row lg:items-start lg:justify-between">
+        <div className="flex flex-col gap-[5.5rem] lg:flex-row lg:items-start lg:justify-between">
           <div>
             <img
               src={logo}
